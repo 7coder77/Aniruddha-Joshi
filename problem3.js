244f9055eb2a47f60a365deefc0542293fa36bde
Aniruddha Joshi <96888738+7coder77@users.noreply.github.com>
2025-06-25 18:29:18 +0000

    problem 3 comment

--- problem3.js
+++ problem3.js
@@ -1,13 +1,15 @@
-// Problem-2: With a single integer as the input, generate the following until a = x [series of numbers as shown in below examples]
- 
+//Problem-3: With a single integer as the input, generate the following until a = x [series of numbers as shown in below examples]
 //   Output: (examples)
 //     1) input a = 1, then output : 1
-//     2) input a = 2, then output : 1, 3
+//     2) input a = 2, then output : 1
 //     3) input a = 3, then output : 1, 3, 5
-//     4) input a = 4, then output : 1, 3, 5, 7
+//     4) input a = 4, then output : 1, 3, 5
+//     5) input a = 5, then output : 1, 3, 5, 7, 9
+//     6) input a = 6, then output : 1, 3, 5, 7, 9
 //     .
 //     .
-//     5) input a = x, then output : 1, 3, 5, 7, .......
+//     7) input a = x, then output : 1, 3, 5, 7, .......
+
 
 // Approach:
 // i created a function that takes a number as input inside the function i initialized an array to store the output
